89cc702fade1da2320bd82638a84cd4ab08d5f0e
MiguelFullStack <miguel.fullstack@gmail.com>
2024-07-30 19:56:49 -0400

    hey

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContext = createContext();
 export const SocketProvider = ({ children }) => {
 
     const { auth, removeCookie } = useContext( AuthContext )
-    const { socket, conectarSocket, desconectarSocket } = useSocket('http://localhost:3001/', auth)
+    const { socket, conectarSocket, desconectarSocket } = useSocket('https://huggopaneloficial.online/', auth)
     
     useEffect(() => {
         if ( auth ) conectarSocket()
